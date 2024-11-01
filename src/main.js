1beb3d320abeedba9c1c9230467edf61898fe6fb
Muotoh <muotricia1@gmail.com>
2024-11-01 10:36:05 +0100

    added css and also styled my landing page

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// import './assets/main.css'
+import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 // import App from './App.vue'
 import router from './router'
 import LandingPage from './views/LandingPage.vue'
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const app = createApp(LandingPage)
 
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="container-fluid">
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel slide" data-bs-ride="carousel" data-bs-interval="2000" data-bs-pause="false">
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <img src="/images/campaign.avif" class="d-block w-100" />
+                    <div class="carousel-caption-overlay">
+
+                    </div>
                 </div>
                 <div class="carousel-item">
                     <img src="/images/campaign10.avif" class="d-block w-100" />
@@ -13,6 +16,191 @@
                 </div>
             </div>
         </div>
+
+
+        <!-- body text for the landing page -->
+        <div class="container-fluid">
+            <div>
+                <p>"Unleash Your Potential by joining Our Mission to Create Lasting Change!" "Be part of something
+                    bigger than yourself. With every contribution, you are helping to build a brighter future.
+                    Together, we are unstoppable." Join the Movement and Make a Difference Today.
+                </p>
+                <h1>About Us</h1>
+                <p>
+                    We are on a mission to protect the environment, empower underserved communities, promote health and
+                    wellness,
+                    etc. Our team brings together passionate advocates, expert
+                    practitioners, and dedicated volunteers who believe in the power of collective action to solve
+                    today's most
+                    pressing issues.
+                    Through our program we are making a measurable impact. Every action counts, and with your help, we
+                    can scale our
+                    efforts and reach more people. Join us as we pave the way for a sustainable, equitable, and thriving
+                    future for
+                    all.
+                </p>
+                <p>
+                    Why Your Contribution Matters
+                    "Every dollar you give goes directly towards outreach, innovation, education, etc. Here is how your
+                    support helps us make a difference:
+                    Impact: We use your contributions to fund projects with a proven track record of success. From
+                    planting trees or building schools to other impactful actions relevant to the campaign, we are
+                    maximizing every resource.
+                    Transparency: We believe in accountability. Our donors receive regular updates on project milestones
+                    and real-time data on the impact of their donations.
+                    Community: When you donate, you are not just a contributor; you are a valued member of our
+                    community. Connect with like-minded individuals, participate in events, and celebrate the progress
+                    we are achieving together."
+                </p>
+                <h1>Our Vision</h1>
+                <p>
+                    We envision a future where every child has access to education, renewable energy. Together, we can
+                    make it happen.
+                </p>
+                <h1>Goals</h1>
+                <p>
+                    Make a Donation: Every dollar brings us closer to our mission.
+                    Volunteer with Us: Join our team and help bring our initiatives to life.
+                    Spread the Word: Share our message with your network to amplify our impact.
+                    Together, we are not just supporting a cause we are building a movement that will shape the future.
+                    Join us and
+                    make a difference."
+                </p>
+                <h1>Testimonials</h1>
+                <p>
+                    It is inspiring to see the real impact of my contributions through us. I feel part of something
+                    truly transformative. Volunteering with us has been a life-changing experience. I have seen
+                    firsthand the positive changes
+                    we are making in people's lives.
+                </p>
+            </div>
+        </div>
+        
+
+        <!-- Footer -->
+        <footer class="text-center text-lg-start text-white" style="background-color: #1c2331">
+            <!-- Section: Social media -->
+            <section class="d-flex justify-content-between p-4" style="background-color: #6351ce">
+                <!-- Left -->
+                <div class="me-5">
+                    <span>Get connected with us on social networks:</span>
+                </div>
+                <!-- Left -->
+
+                <!-- Right -->
+                <div>
+                    <a href="" class="text-white me-4">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="" class="text-white me-4">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="" class="text-white me-4">
+                        <i class="fab fa-google"></i>
+                    </a>
+                    <a href="" class="text-white me-4">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="" class="text-white me-4">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                    <a href="" class="text-white me-4">
+                        <i class="fab fa-github"></i>
+                    </a>
+                </div>
+                <!-- Right -->
+            </section>
+            <!-- Section: Social media -->
+
+            <!-- Section: Links  -->
+            <section class="">
+                <div class="container text-center text-md-start mt-5">
+                    <!-- Grid row -->
+                    <div class="row mt-3">
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                            <!-- Content -->
+                            <h6 class="text-uppercase fw-bold">Company name</h6>
+                            <hr class="mb-4 mt-0 d-inline-block mx-auto"
+                                style="width: 60px; background-color: #7c4dff; height: 2px" />
+                            <p>
+                                Here you can use rows and columns to organize your footer
+                                content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                                elit.
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold">Products</h6>
+                            <hr class="mb-4 mt-0 d-inline-block mx-auto"
+                                style="width: 60px; background-color: #7c4dff; height: 2px" />
+                            <p>
+                                <a href="#!" class="text-white">MDBootstrap</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-white">MDWordPress</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-white">BrandFlow</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-white">Bootstrap Angular</a>
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold">Useful links</h6>
+                            <hr class="mb-4 mt-0 d-inline-block mx-auto"
+                                style="width: 60px; background-color: #7c4dff; height: 2px" />
+                            <p>
+                                <a href="#!" class="text-white">Your Account</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-white">Become an Affiliate</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-white">Shipping Rates</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-white">Help</a>
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold">Contact</h6>
+                            <hr class="mb-4 mt-0 d-inline-block mx-auto"
+                                style="width: 60px; background-color: #7c4dff; height: 2px" />
+                            <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+                            <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
+                            <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+                            <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                        </div>
+                        <!-- Grid column -->
+                    </div>
+                    <!-- Grid row -->
+                </div>
+            </section>
+            <!-- Section: Links  -->
+
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+                © 2020 Copyright:
+                <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+            </div>
+            <!-- Copyright -->
+        </footer>
+        <!-- Footer -->
+
+
     </div>
 </template>
 
@@ -24,4 +212,22 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+
+.carousel-caption-overlay {
+    position: absolute;
+    bottom: 0;
+    top: 0;
+    left: 0;
+    right: 0;
+    background: rgba(0, 0, 0, 0.7);
+    color: white;
+    text-align: center;
+}
+
+.carousel-item>img {
+    width: 100%;
+
+    height: 730px;
+}
+</style>
