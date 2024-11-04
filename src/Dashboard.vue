877a55e105e6501516188a923c2fdb07067a2435
Adedoyin Sodiq <sodiqadedoyin12@gmail.com>
2024-11-04 14:41:58 +0100

    Genesis commit: dashboard infinion_cohort project

---
+++ src/Dashboard.vue
@@ -0,0 +1,15 @@
+<script setup>
+import SideBarComp from "./components/SideBarComp.vue";
+</script>
+
+
+<template>
+  <SideBarComp />
+
+
+
+
+
+  
+  <router-view></router-view>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
-import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-const app = createApp(App)
+// import App from './App.vue'
+import router from "./router";
+import Dashboard from "./Dashboard.vue";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(Dashboard);
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
-app.mount('#app')
+app.use(vuetify);
+app.use(router);
+app.use(createPinia());
+
+app.mount("#app");
