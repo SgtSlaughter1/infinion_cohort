ddb0ba3ab71e4081852fb6b24c9d690cdfecc1ed
samthatcode <samthatcode@gmail.com>
2024-11-07 18:03:17 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import OverviewComp from '@/views/OverviewComp.vue';
-import NewCampaign from '@/views/NewCampaign.vue'
-
-
 import LandingPage from '@/views/LandingPage.vue'
 import LoginPage from '@/views/LoginPage.vue'
 import SignUp from '@/views/SignUp.vue'
@@ -15,17 +12,17 @@ import Campaign from '@/views/Campaign.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-  
-    {
-      path: '/',
-      name: 'OverviewComp',
-      component: OverviewComp
+      {
+      path: '',
 
-      name: 'landingPage',
-      component: LandingPage     
-
+       name: 'landingPage',
+      component: LandingPage  
     },
-    
+    {
+      path: '/overview',
+      name: 'OverviewComp',
+      component: OverviewComp       
+    },    
     {
 
       path: "/NewCampaign",
