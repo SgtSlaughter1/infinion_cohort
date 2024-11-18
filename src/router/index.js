495561858c59562946f09abf3095c408d4a34b51
Abdulsalam <habeebabdulsalam86@gmail.com>
2024-11-18 12:04:30 +0100

    Update: Corrected the route paths

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import AllCampaign from "/src/views/AllCampaign.vue";
 import EditCampaign from "/src/views/EditCampaign.vue";
 import ViewCampaign from "@/views/ViewCampaign.vue";
 import LandingPage from "@/views/LandingPage.vue";
 import LoginPage from "@/views/LoginPage.vue";
 import SignUp from "@/views/SignUp.vue";
-import Overview from "@/views/Overview.vue";
-import NewCampaign from "@/views/NewCampaign.vue";
-import Campaign from "@/views/Campaign.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
